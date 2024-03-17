cc0258ca7c753d36cddad6896567a1c83854912f
Akila Ishan Meegoda <akilaishanmeegoda@gmail.com>
2024-03-17 19:52:54 +0530

    concurrently update

--- Event management/frontend/src/pages/Reservations/client_dashboard/InitialPayment.jsx
+++ Event management/frontend/src/pages/Reservations/client_dashboard/InitialPayment.jsx
@@ -1,10 +1,12 @@
 import { Button, Label, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import avatar from "../../../images/profile.jpg";
+import payment from "../../../images/payment.jpg";
+import upload from "../../../images/upload.jpg";
 import "../../../App.css";
 
 const InitialPayment = () => {
+  const [fileUploaded, setFileUploaded] = useState(false);
   const location = useLocation();
   const clientName = location.state.clientName;
   const clientEmail = location.state.clientEmail;
@@ -48,6 +50,8 @@ const InitialPayment = () => {
   };
 
   const handleFileUpload = async (e) => {
+    setFileUploaded(true);
+    
     const file = e.target.files[0];
     const base64 = await convertToBase64(file);
     setPostImage({ ...postImage, receipt: base64 });
@@ -55,20 +59,73 @@ const InitialPayment = () => {
 
   return (
     <div>
-      <form onSubmit={handleCreateReservation}>
-        <label htmlFor="file-upload" className="custom-file-upload">
-          <img src={postImage.receipt || avatar} alt="" />
-        </label>
-        <input
-          type="file"
-          label="Image"
-          name="receipt"
-          id="file-upload"
-          accept=".jpeg,.png,.jpg"
-          onChange={(e) => handleFileUpload(e)}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+      <div className="my-4 px-44">
+        <div className="flex justify-between p-6 mb-6 rounded-xl bg-client-brown">
+          <h2 className="text-3xl font-bold text-white ">Initial Payment</h2>
+        </div>
+        <div className="flex">
+          <div className="size-[600px]">
+            <img src={payment} alt="payment img" />
+            <p className="font-bold text-center">Upload here</p>
+            <div className="pl-[275px]">
+              <label
+                htmlFor="file-upload"
+                className="m-auto custom-file-upload "
+              >
+                <img src={upload} alt="" />
+              </label>
+              <input
+                className="bg-black"
+                type="file"
+                label="Image"
+                name="receipt"
+                id="file-upload"
+                accept=".jpeg,.png,.jpg"
+                onChange={(e) => handleFileUpload(e)}
+              />
+            </div>
+
+          </div>
+          <form onSubmit={handleCreateReservation}>
+            <div className="ml-24 bg-client-yellow size-[350px] rounded-xl shadow-2xl">
+              <div className="w-full h-12 shadow-2xl rounded-xl bg-client-brown">
+                <h1 className="pt-2 ml-4 font-bold text-white ">
+                  Bank Details
+                </h1>
+              </div>
+              <div className="p-4">
+                <p className="text-2xl font-bold text-client-brown">
+                  Bank Name
+                </p>
+                <p className="mb-2 text-xl italic font-bold text-black">
+                  Commercial Bank
+                </p>
+                <p className="text-2xl font-bold text-client-brown">
+                  Account Number
+                </p>
+                <p className="mb-2 text-xl italic font-bold text-black">
+                  801 666 1230
+                </p>
+                <p className="text-2xl font-bold text-client-brown">
+                  Branch Location
+                </p>
+                <p className="mb-2 text-xl italic font-bold text-black">
+                  Kalutara
+                </p>
+                <p className="text-2xl font-bold text-client-brown">
+                  Branch Code
+                </p>
+                <p className="mb-2 text-xl italic font-bold text-black">024</p>
+              </div>
+            </div>
+            <p className="mt-8 ml-24 font-bold text-center text-red-600">Minimum payment amount is Rs.1000/=</p>
+            <p className="ml-24 font-bold text-center text-red-600 ">(Please upload jpg , png , jpeg types only.)</p>
+            <Button className="mt-4 shadow-xl ml-52 bg-client-brown" type="submit" disabled={!fileUploaded}>
+              Upload Slip
+            </Button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
