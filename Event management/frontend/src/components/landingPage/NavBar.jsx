ca44e31925816de794cf00dbdbd2ba929c85ed1b
Akila Ishan Meegoda <akilaishanmeegoda@gmail.com>
2024-03-19 12:18:26 +0530

    Update NavBar.jsx

--- Event management/frontend/src/components/landingPage/NavBar.jsx
+++ Event management/frontend/src/components/landingPage/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
                 name@flowbite.com
               </span>
             </Dropdown.Header>
-            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Item href="/admin/dashboard">Dashboard</Dropdown.Item>
             <Dropdown.Item>Settings</Dropdown.Item>
             <Dropdown.Item>Earnings</Dropdown.Item>
             <Dropdown.Divider />
